546849f4e0bfc9302e3ab8faa97e0d899ef1124c
Sr-Kaio <caioprobr6@gmail.com>
2024-05-29 20:28:30 -0300

    a

--- src/js/basic.js
+++ src/js/basic.js
@@ -1,31 +1,17 @@
+
+window.onload = function () {
+    var xmlhttp = new XMLHttpRequest();
+    var url = "https://api.mcstatus.io/v2/status/java/BBS1D9rj.aternos.me";
+    xmlhttp.open("GET", url, true);
+
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var json = JSON.parse(this.responseText);
+            console.log(json.players.online)
+        }
+    };
+};
+
 function basicCopy(textToCopy) {
     navigator.clipboard.writeText(textToCopy);
 }
-
-    // Set the date we're counting down to
-    var countDownDate = new Date("May 29, 2024 19:50:00").getTime();
-
-    // Update the count down every 1 second
-    var x = setInterval(function() {
-
-        // Get today's date and time
-        var now = new Date().getTime();
-
-        // Find the distance between now and the count down date
-        var distance = countDownDate - now;
-
-        // Time calculations for days, hours, minutes and seconds
-        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-        // Display the result in the element with id="demo"
-        document.getElementById("cooldown_").innerHTML = hours + "h "
-        + minutes + "m " + seconds + "s ";
-
-        // If the count down is finished, write some text
-        if (distance < 0) {
-            clearInterval(x);
-            document.getElementById("cooldown_").innerHTML = "Servidor lançado...";
-        }
-    }, 1000);
