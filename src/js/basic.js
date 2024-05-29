d0fb8f717d01c33f7c4613eecfac5a325cf1d9d4
Sr-Kaio <caioprobr6@gmail.com>
2024-05-29 20:31:44 -0300

    a

--- src/js/basic.js
+++ src/js/basic.js
@@ -7,7 +7,7 @@ window.onload = function () {
     xmlhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             var json = JSON.parse(this.responseText);
-            console.log(json.players.online)
+            document.getElementById("playeramount").innerHTML = json.players.online
         }
     };
 };
